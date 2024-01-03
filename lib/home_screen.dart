82abf185912efbb450f0e88d5daca5e647695f60
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2024-01-03 12:00:39 +0545

    Implement basic UI.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _progress = true;
+  bool _loggedIn = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: _buildBody(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(child: _buildBody()),
+      ),
     );
   }
 
@@ -18,6 +29,30 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _buildBody() {
-    return Container();
+    if (_progress) {
+      return _buildProgressWidget();
+    } else if (_loggedIn) {
+      return _buildTokenRenewWidget();
+    } else {
+      return _buildLoginWidget();
+    }
+  }
+
+  Widget _buildProgressWidget() {
+    return const CircularProgressIndicator();
+  }
+
+  Widget _buildLoginWidget() {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text("Login"),
+    );
+  }
+
+  Widget _buildTokenRenewWidget() {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text("Simulate Token Refresh"),
+    );
   }
 }
